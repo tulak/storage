f119495169ab94b5ef13c49058cab5c49ead565f
Prelang Builder <builder@prelang.com>
2014-09-10 19:28:54 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   has_many :tools
+  belongs_to :division
 end
